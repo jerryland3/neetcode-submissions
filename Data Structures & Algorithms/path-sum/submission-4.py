0398a2a787b993ee8df5c09a7c870d75f37ecb7d
Jerry <jwang499@jh.edu>
2026-08-22 18:17:40 -0700

    Add: path-sum - submission-4

---
+++ Data Structures & Algorithms/path-sum/submission-4.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def hasPathSumHelper(root: Optional[TreeNode], current_sum: int) -> bool:
+            if not root:
+                return False
+
+            current_sum += root.val             
+            if not root.left and not root.right:
+                return current_sum == targetSum
+
+            return hasPathSumHelper(root.left, current_sum) or hasPathSumHelper(root.right, current_sum)
+
+
+        return hasPathSumHelper(root, 0)
+
+
+"""
+        1
+
+    1         0
+
+1
+
+time complexity O(n) since we need to traverse the entire tree in worst case
+space compleixty is also O(h) where h is the height of the tree
+                
+"""  
+        
