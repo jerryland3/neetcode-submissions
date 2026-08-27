8e9087f6264d1b237994ed5ef77ec9b5a26c6fbf
Jerry <jwang499@jh.edu>
2026-08-26 22:40:57 -0700

    Add: permutations - submission-1

---
+++ Data Structures & Algorithms/permutations/submission-1.py
@@ -0,0 +1,50 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        output = []
+        curList = []
+        length = len(nums)
+        used = [False for _ in range(length)]
+
+        def helper(usedList: List[bool], index: int):
+            if index >= length:
+                output.append(curList[:])
+                return
+            
+            for j in range(length):
+                if not usedList[j]:
+                    curList.append(nums[j])
+                    usedList[j] = True
+                    helper(usedList, index + 1)
+                    curList.pop()
+                    usedList[j] = False
+        
+        helper(used, 0)
+        return output
+    
+
+"""
+nums = [1,2,3]
+
+                        []
+             1          2           3          0
+          2    3      1   3       1   2        1
+          3    2      3   1       2   1        2
+
+    [T,F,F]   [T,F,F]
+    [T,T,F]   [T,F,T]
+    [T,T,T]   [T,T,T]
+
+base case:
+    - index >= len(nums), append copy of curList to output and return
+
+recursive case:
+    - loop through j from 0 to len(nums)-1
+    - check if index j of truth table is False. If so, append nums[j] into curList and set truth table to True. If not, continue
+    - recursivly go into next level with truth list and index + 1
+    - restore truth list by setting index j to False and pop from curList
+
+complexity:
+    - n! choices and each permutation requires n list append. Time complexity of O(n*n!)
+    - curList appending happen in place. Recursion stack is at most n levels. Thus need O(n) aux space and O(n!) output space.
+"""
+        
