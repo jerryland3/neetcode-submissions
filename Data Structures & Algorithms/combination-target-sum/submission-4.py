c1ca10f9cef2f5366c31332bbd47046e3978b18a
Jerry <jwang499@jh.edu>
2026-09-01 20:03:22 -0700

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.py
@@ -0,0 +1,51 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        output = []
+        curList = []
+        length = len(nums)
+
+        def helper(index: int, curSum: int):
+            if curSum == target:
+                output.append(curList[:])
+                return
+            if curSum > target or index == length:
+                return
+            
+            for j in range(index, length):
+                curSum += nums[j]
+                curList.append(nums[j])
+                helper(j, curSum)
+                curSum -= nums[j]
+                curList.pop()
+        
+        helper(0, 0)
+        return output
+
+"""
+nums = [2,5,6], target = 6
+                                                []
+        2                                 5           6        
+    2       5      6             
+  2 5 6    5 6     6          
+
+[5]
+
+base case:
+    - if path sum == target, then add to output list
+    - if path sum > target, then just return
+
+recursive case:
+    - loop through every index, starting from current index and onward
+    - update path sum
+    - add element of current index into curList
+    - recursivly traverse down the tree
+    - pop element from curList
+
+complexity:
+    - time complexity of O(n * n/(k)) = O(n^2/k) where k is the smallest number in nums. This
+      is because there is at most n/k layers in the tree, and at each layer we need to process
+      at most n elements.
+
+    - space complexity of O(n/k) since the stack is at most n/k deep 
+"""
+
