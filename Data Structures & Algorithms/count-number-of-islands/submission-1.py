cafcb8a6342ba386aae29e5cfafdddf855251131
Jerry <jwang499@jh.edu>
2026-09-15 20:48:48 -0700

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,46 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0
+        maxRow = len(grid)
+        maxCol = len(grid[0])
+        seen = set()
+
+        def DFS(row: int, col: int):
+            if (min(row, col) < 0 or row >= maxRow or col >= maxCol or
+               ((row, col) in seen or grid[row][col] == '0')
+            ):
+                return
+            
+            seen.add((row, col))
+            for rowDelta, colDelta in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                DFS(row + rowDelta, col + colDelta)
+            
+            return
+
+        for rowIndex in range(maxRow):
+            for colIndex in range(maxCol):
+                if ((rowIndex, colIndex) not in seen and 
+                     grid[rowIndex][colIndex] == '1'
+                   ):
+                    DFS(rowIndex, colIndex)
+                    count += 1
+
+        return count 
+
+"""
+[
+    [1 1 0 1]
+    [1 1 0 0]
+    [0 0 1 1]
+]
+
+strategy:
+    - loop through every grid and use DFS. If we land on water or already seen land, then just return 0. If it is
+      new land, use DFS and mark all connected land, then return 1 for count.
+    
+    - increment count after each DFS search.
+
+Complexity:
+    - time complexity of O(mxn) since we will visit each grid at most once and each visit is O(1)
+    - Aux space is O(mxn) due to the set and recursion stack. Output space is O(1)
+"""
