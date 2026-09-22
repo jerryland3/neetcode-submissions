03a5f0e96a4da0dd178dd9df3eaec88ecdfe3696
Jerry <jwang499@jh.edu>
2026-09-21 23:11:21 -0700

    Add: rotting-fruit - submission-5

---
+++ Data Structures & Algorithms/rotting-fruit/submission-5.py
@@ -0,0 +1,69 @@
+from collections import deque
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        queue = deque()
+        seen = set()
+        maxRow = len(grid)
+        maxCol = len(grid[0])
+
+        def freshCount(board: list[list[int]]):
+            fresh = 0
+            for row in range(maxRow):
+                for col in range(maxCol):
+                    if board[row][col] == 1 and (row, col) not in seen:
+                        fresh += 1
+            return fresh
+
+        def initalSearch():
+            for row in range(maxRow):
+                for col in range(maxCol):
+                    if grid[row][col] == 2:
+                        queue.append((row, col))
+        
+        def BFS():
+            length = 0
+            while queue:
+                lengthQueue = len(queue)
+                length += 1
+                for _ in range(lengthQueue):
+                    row, col = queue.popleft()
+                    for rowDiff, colDiff in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+                        newRow = row + rowDiff
+                        newCol = col + colDiff
+                        if (min(newRow, newCol) < 0 or newRow >= maxRow or newCol >= maxCol or
+                            grid[newRow][newCol] == 0 or grid[newRow][newCol] == 2 or (newRow, newCol) in seen):
+                            continue
+                        seen.add((newRow, newCol))
+                        queue.append((newRow, newCol))
+
+            return length - 1
+        
+        if freshCount(grid) == 0:
+            return 0
+        
+        initalSearch()
+        time = BFS()
+
+        if freshCount(grid) > 0:
+            return -1
+        
+        return time
+                        
+
+
+
+"""
+Strategy:
+    - inital search of all grid to find rotten orange and place it in BFS queue
+    - use BFS search and return the length of the search
+    - finial search of all grid to see if there are anymore fresh organge
+        if there are, return -1, else return length from BFS
+
+Complexity:
+    - O(mxn) time since inital search, final search, and BFS are O(mxn)
+    - O(mxn) aux space for BFS queue, and O(1) for output space 
+
+5
+[(0,1)]
+"""
+        
